930e4d0e8d4bc9d631fdcb2c6f463e970e672544
devry12 <devrykawiryan@gmail.com>
2024-10-20 17:05:00 +0700

    update command and fix folder

--- flutter_kreasi.rb
+++ flutter_kreasi.rb
@@ -2,7 +2,7 @@ class FlutterKreasi < Formula
     desc "Tool for initializing Flutter projects"
     homepage "https://kreasimaju.id"
     url "https://kreasimaju.id/brew/flutter_kreasi"
-    version "0.1.0"
+    version "1.0.0"
     sha256 "fdb27281beb0e5ddea254ae8e9d5d5bce3b5fb710c06c55afe73970552a1011e" # Ganti dengan SHA256 yang benar
 
     def install
