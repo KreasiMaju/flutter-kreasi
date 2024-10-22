c316d6917fe51c46a58e66c9eb9fbf02e72664b2
devry12 <devrykawiryan@gmail.com>
2024-10-22 13:59:31 +0000

    Bump version to 1.0.3 and update URL

--- Formula/flutter_kreasi.rb
+++ Formula/flutter_kreasi.rb
@@ -1,8 +1,8 @@
 class FlutterKreasi < Formula
     desc "Tool for initializing Flutter projects"
     homepage "https://kreasimaju.id"
-    url "https://github.com/KreasiMaju/flutter-kreasi/releases/download/1.0.2/flutter_kreasi"
-    version "1.0.2"
+    url "https://github.com/KreasiMaju/flutter-kreasi/releases/download/1.0.3/flutter_kreasi"
+    version "1.0.3"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Ganti dengan SHA256 yang benar
 
     def install
